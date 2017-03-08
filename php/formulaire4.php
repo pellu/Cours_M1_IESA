aca7fcc213df3f23e191fd7d7aaf0507415cbff2
pellu <jordanpelluard@gmail.com>
2017-03-08 12:18:48 +0100

    error mail

--- php/formulaire4.php
+++ php/formulaire4.php
@@ -1,6 +1,11 @@
 <?php
-if($_POST){
+if($_POST AND empty($_POST['mail'])){
+	echo "<h2>champs mail vide</h2><br>";
+}else{
 	echo "pseudo : " .$_POST['pseudo']."<br>";
 	echo "mail : " .$_POST['mail']."<br>";
+}
+foreach ($_POST as $indice => $valeur) {
+	echo "$indice :valeur";
 }
 ?>
